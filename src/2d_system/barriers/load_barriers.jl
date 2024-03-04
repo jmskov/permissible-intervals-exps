118c91c7b596aee47eff8febae110a0fca3c4451
John Skovbekk <jmcjack@gmail.com>
2024-03-04 15:04:53 -0600

    update checks

--- src/2d_system/barriers/load_barriers.jl
+++ src/2d_system/barriers/load_barriers.jl
@@ -1,30 +1,43 @@
 using MAT
 
-beta_filename = "beta_dual73.mat"
-indeces_filename = "indices.mat"
+beta_filename = joinpath(@__DIR__, "beta_dual196.mat")
+indeces_filename = joinpath(@__DIR__, "indices196.mat")
 
 beta = matread(beta_filename)["beta_dual"] # 5x100 matrix... what is the order of the cols? will it follow the order of the states?
 
 indeces = matread(indeces_filename)["index_matrix"] # indeces is column of controls, with and the corresponding state index. these are the removed indeces
 
-beta_filter = [beta[idx...] for idx in eachrow(indeces)]
-
 # create a new dict of state to admissible control indeces
-admissible_controls = Dict{Int, Vector{Int}}()
-
+admissible_controls = Dict{Int, Vector{Int}}
+# add all the control indeces to the dict (1:5 for each state)
 for i=1:100
     admissible_controls[i] =  collect(1:5)
 end
+# at the same time, create a dict version of the list
+removed_controls = Dict{Int, Vector{Int}}()
 
 num_zero = 0
 for i=1:size(indeces, 1)
     global num_zero
-        deleteat!(admissible_controls[indeces[i,2]], admissible_controls[indeces[i,2]] .== indeces[i,1])
-        if isempty(admissible_controls[indeces[i,2]])
-            num_zero += 1
-        end
+    deleteat!(admissible_controls[indeces[i,2]], admissible_controls[indeces[i,2]] .== indeces[i,1])
+    if isempty(admissible_controls[indeces[i,2]])
+        num_zero += 1
+    end
+
+    if indeces[i,2] ∈ keys(removed_controls)
+        push!(removed_controls[indeces[i,2]], indeces[i,1])
+    else
+        removed_controls[indeces[i,2]] = [indeces[i,1]]
+    end
 end
 @assert num_zero == 0
+
+zero_states = []
+for (k, v) in admissible_controls
+    if isempty(v)
+        push!(zero_states, k)
+    end
+end
 
 # now define the initial state range
 # initial_state_range = [0.4:0.5, 0.4:0.5]
